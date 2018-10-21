81c2c92e66f0ae9ee4094aab43f1abcf81c4882b
kiriharu <kiriharu@github.com>
2018-10-21 15:31:26 +0300

    fix #1 и некоторые доработки

--- message_edit.py
+++ message_edit.py
@@ -3,7 +3,7 @@
 import requests, time
 def call(method, options={}, **kwargs):
     '''Фукнция вызова api ВК.'''
-    options['access_token'] ='Сюда токен'
+    options['access_token'] = token 
     options['v'] = '5.73'
     options.update(kwargs)
     resp = requests.get('https://api.vk.com/method/'+method, params=options).json()
@@ -11,35 +11,36 @@ def call(method, options={}, **kwargs):
         print('VKERROR: {error_code}: {error_msg}'.format(**resp['error']))
     return resp
 
-def send_message(id, message):
+def send_message(peer_id, message):
     '''Функция отправки сообщений.'''
     options = {
         'message' : message,
-        'peer_id' : id,
+        'peer_id' : peer_id,
     }
     message_id = call('messages.send', options)['response']
-    print('Message {0} sended'.format(message))
-    print(message_id)
+    print('Сообщение {} отправлено'.format(message))
+    print('Работаем с сообщением с ID {}'.format(message_id))
     return message_id
 
-def main():
+def main(peer_id, message):
     '''Изменяет сообщение побуквенно и выводит его
-    id - id беседы
+    peer_id - id беседы
     message - любое сообщение'''
-    id = input("Enter id: ")
-    message = input("Enter message: ")
     lastmessage = message[0]
-    message_id = send_message(id, lastmessage)
+    message_id = send_message(peer_id, lastmessage)
     for i in message[1:]:
         lastmessage += i
         options = {
-            'peer_id': id,
+            'peer_id': peer_id,
             'message': lastmessage,
             'message_id': message_id
         }
-        print(i)
+        print('Добавлена буква {}'.format(i))
         time.sleep(1)
         call('messages.edit', options)
 
 if __name__ == '__main__':
-    main()
+    token = input("Введите токен: ")
+    peer_id = input("Введите ID чата: ")
+    message = input("Введите сообщение: ")
+    main(peer_id, message)
