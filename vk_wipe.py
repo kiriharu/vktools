05440c5d6f94f21b407a7af8d3e3c4ee2122abbe
kiriharu <kiriharu@github.com>
2018-10-21 15:27:53 +0300

    Рефактор

--- vk_wipe.py
+++ vk_wipe.py
@@ -1,16 +1,11 @@
 # coding = utf-8
-'''Вайпалка, дорогие диванные воены. Ваш опонент не прав и не хочет слушать вас? Пускай выслушает стену текста!
-Есть 5 режима вайпа: вайп обычным текстом, вайп картинкой, вайп картинкой и текстом, вайп рандомным текстом и картинкой, вайп рандомным текстом.
-В случае рандомного текста - он конкатенируется с предыдущим.'''
+'''Вайпалка, дорогие диванные воены. '''
 import requests
 import random
-from datetime import time
 import time
-
-access_token = input("Enter access_token: ")
+import string
 
 def call(method, options={}, **kwargs):
-    global access_token
     options['access_token'] = access_token
     options['v'] = '5.73'
     options.update(kwargs)
@@ -19,113 +14,85 @@ def call(method, options={}, **kwargs):
         print('VKERROR: {error_code}: {error_msg}'.format(**resp['error']))
     return resp
 
-def send_message(id, textmessage='',photovar=''):
+def send_message(peer_id, textmessage='',photovar=''):
     options = {
         'message' : textmessage,
-        'peer_id' : id,
+        'peer_id' : peer_id,
     }
     if photovar != '':
         options['attachment'] = 'photo' + photovar
     call('messages.send', options)
-    print('Message' + '(' + textmessage + ')' + 'and photo' + photovar + 'send to' + '(' + id + ')' )
-    return
+    print('Отправлено {textmessage} и {photovar} в {peer_id}'.format(textmessage = textmessage, photovar = photovar, peer_id = peer_id))
 
-def wipe_text(messagecount, id, timer):
-    textmessage = input("Enter message to send(if nothing entered message text is wipe): ")
-    i = 0
-    while i < int(messagecount):
-        try:
-            time.sleep(int(timer))
-            send_message(id, textmessage)
-            i = i + 1
-        except Exception as e:
-            print(e)
-def wipe_pictures(messagecount, id, timer):
-    photomode = input("Enter photomode: \n 1) Post choosed photo \n 2) Post random photo \n")
+def stringGen(length):
+	textmessage = ''
+	for _ in range(int(length)):
+		textmessage += random.choice(string.printable)
+	return textmessage
+
+def wipe_text(messagecount, peer_id, timer, textmode, length=1, textmessage='sometext'):
+	if textmode == '1':
+	    for _ in range(messagecount):
+	        try:
+	            time.sleep(timer)
+	            send_message(peer_id, textmessage)
+	        except Exception as e:
+	            print(e)
+	elif textmode == '2':
+		for _ in range(messagecount):
+			try:
+				textmessage = stringGen(length)
+				time.sleep(timer)
+				send_message(peer_id, textmessage)
+			except Exception as e:
+				print(e)
+
+def wipe_pictures(messagecount, peer_id, timer, photomode, photo='<owner_id>_<media_id>', album='saved'):
     if photomode == '1':
-        photo = input("Enter id photo in <owner_id>_<media_id> type: ")
-        i = 0
-        while i < int(messagecount):
+        for _ in range(messagecount):
             try:
-                time.sleep(int(timer))
-                send_message(id, '', photo)
-                i = i + 1
+                time.sleep(timer)
+                send_message(peer_id, '', photo)
             except Exception as e:
                 print(e)
-    if photomode == '2':
-        i = 0
-        while i < int(messagecount):
+    elif photomode == '2':
+        for _ in range(messagecount):
             try:
-                time.sleep(int(timer))
-                photos = call('photos.get', album_id='saved')['response']['items']
+                time.sleep(timer)
+                photos = call('photos.get', album_id=album)['response']['items']
                 random.shuffle(photos)
-                send_message(id, '', '{owner_id}_{id}'.format(**random.choice(photos)))
-                i = i + 1
+                send_message(peer_id, '', '{owner_id}_{id}'.format(**random.choice(photos)))
             except Exception as e:
                 print(e)
-def wipe_picturestext(messagecount, id, timer):
-    photo = input("Enter id photo in <owner_id>_<media_id> type: ")
-    textmessage = input("Enter message to send(if nothing entered message text is wipe): ")
-    i = 0
-    while i < int(messagecount):
-        try:
-            time.sleep(int(timer))
-            send_message(id, textmessage, photo)
-            i = i + 1
-        except Exception as e:
-            print(e)
-
-def wipe_picturesrandomtext(messagecount, id, timer):
-    photo = input("Enter id photo in <owner_id>_<media_id> type: ")
-    chars = list("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&amp;*()")
-    length = input("Input message length")
-    textmessage = ''
-    i = 0
-    while i < int(messagecount):
-        try:
-            for _ in range(int(length)):
-                textmessage += random.choice(chars)
-            time.sleep(int(timer))
-            send_message(id, textmessage, photo)
-            i = i + 1
-        except Exception as e:
-            print(e)
-
-def wipe_randomtext(messagecount, id, timer):
-    chars = list("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&amp;*()")
-    length = input("Input message length")
-    i = 0
-    textmessage = ''
-    while i < int(messagecount):
-        try:
-            for _ in range(int(length)):
-                textmessage += random.choice(chars)
-            time.sleep(int(timer))
-            send_message(id, textmessage)
-            i = i + 1
-        except Exception as e:
-            print(e)
 
 def main():
-    print('Welcome to WIPE MACHINE by Kiriharu! All fucking rights recived')
-    messagecount = input("Enter count of messages to post: ")
-    id = input("Enter chat id: ")
-    timer = input("Enter timer in seconds: ")
-    option = input("Choose you way: \n 1) Text wipe \n 2) Picture wipe(included random mode) \n 3) Text and picture wipe \n 4) Random text and picture wipe \n 5) Random text wipe \n")
+    print('Вайпалка от Kiriharu!')
+    option = input('''Выбери свой путь:
+    1) Вайп текстом (рандомным и выбранным)
+    2) Вайп картинками (из сохранённого альбома и конкретной пикчей)
+    >>>''')
+    messagecount = input('Введите количество сообщений для вайпа: ')
+    peer_id = input('Введите ID чата: ')
+    timer = input('Введите время между постингом: ')
     if option == '1':
-        wipe_text(messagecount,id,timer)
-        main()
+    	textmode = input('Введите режим работы. 1 - спам конкретного сообщения, 2 - спам рандомными сообщениями: ')
+    	if textmode == '1':
+    		textmessage = input('Введите текст для вайпа: ')
+    		wipe_text(int(messagecount), peer_id, int(timer), textmode, textmessage = textmessage)
+    	elif textmode == '2':
+    		length = input('Введите размер генерируемого текста: ')
+    		wipe_text(int(messagecount), peer_id, int(timer), textmode, length = length)
     elif option == '2':
-        wipe_pictures(messagecount,id,timer)
-        main()
-    elif option == '3':
-        wipe_picturestext(messagecount, id, timer)
-        main()
-    elif option == '4':
-        wipe_picturesrandomtext(messagecount, id, timer)
-        main()
-    elif option == '5':
-        wipe_randomtext(messagecount, id, timer)
-        main()
-    return
+    	photomode = input('Введите режим работы. 1 - спам выбранной картинкой, 2 - спам с выбранного альбома.')
+    	if photomode == '1':
+    		photo = input('Введите имя фото в виде <owner_id>_<media_id>: ')
+    		wipe_pictures(int(messagecount), peer_id, int(timer), photomode, photo = photo)
+    	elif photomode == '2':
+    		album = input('Введите название или id альбома. Например, saved: ')
+    		wipe_pictures(int(messagecount), peer_id, int(timer), photomode, album = album)
 
+
+if __name__ == '__main__':
+	access_token = input('Введите access token: ')
+	main()
+	
